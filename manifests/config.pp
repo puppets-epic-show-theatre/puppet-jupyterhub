9eeba18d3f60bfd0d1ef8fe9a4d3d8f93abaa87d
Stephan Eicher <stephan.eicher@check24.de>
2022-10-25 18:34:06 +0200

    Fix linting issues reported by pdk

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,9 +3,8 @@
 # This class is called from jupyterhub for service config.
 #
 class jupyterhub::config {
-
-  file { "${::jupyterhub::jupyterhub_dir}/jupyterhub_config.py":
-    owner   => $::jupyterhub::jupyterhub_username,
+  file { "${jupyterhub::jupyterhub_dir}/jupyterhub_config.py":
+    owner   => $jupyterhub::jupyterhub_username,
     content => template("${module_name}/jupyterhub_config.py.erb"),
   }
 
@@ -14,13 +13,13 @@ class jupyterhub::config {
     content => template("${module_name}/jupyterhub_sudoers.erb"),
   }
 
-  file { "${::jupyterhub::jupyterhub_dir}/start_jupyterhub.sh":
-    owner   => $::jupyterhub::jupyterhub_username,
+  file { "${jupyterhub::jupyterhub_dir}/start_jupyterhub.sh":
+    owner   => $jupyterhub::jupyterhub_username,
     mode    => '0755',
     content => template("${module_name}/start_jupyterhub.sh.erb"),
   }
 
-  file { "/etc/systemd/system/${::jupyterhub::service_name}.service":
+  file { "/etc/systemd/system/${jupyterhub::service_name}.service":
     owner   => 'root',
     content => template("${module_name}/jupyterhub.service.erb"),
   }
--- manifests/install.pp
+++ manifests/install.pp
@@ -3,44 +3,43 @@
 # This class is called from jupyterhub for install.
 #
 class jupyterhub::install {
-
-  file { $::jupyterhub::jupyterhub_dir:
+  file { $jupyterhub::jupyterhub_dir:
     ensure => directory,
-    owner   => $::jupyterhub::jupyterhub_username,
+    owner  => $jupyterhub::jupyterhub_username,
   }
-  ->
-  package { 'python3-venv': }
-  ->
-  python::pyvenv { $::jupyterhub::pyvenv:
+
+  -> package { 'python3-venv': }
+
+  -> python::pyvenv { $jupyterhub::pyvenv:
     ensure  => present,
     version => 'system',
-    owner   => $::jupyterhub::jupyterhub_username,
+    owner   => $jupyterhub::jupyterhub_username,
   }
 
   python::pip { 'jupyter':
     pkgname    => 'jupyter',
-    virtualenv => "$::jupyterhub::pyvenv",
-    owner      => $::jupyterhub::jupyterhub_username,
-    require    => Python::Pyvenv[ $::jupyterhub::pyvenv ],
+    virtualenv => $jupyterhub::pyvenv,
+    owner      => $jupyterhub::jupyterhub_username,
+    require    => Python::Pyvenv[$jupyterhub::pyvenv],
   }
 
   python::pip { 'jupyterhub':
     pkgname    => 'jupyterhub',
-    virtualenv => "$::jupyterhub::pyvenv",
-    owner      => $::jupyterhub::jupyterhub_username,
-    require    => Python::Pyvenv[ $::jupyterhub::pyvenv ],
+    virtualenv => $jupyterhub::pyvenv,
+    owner      => $jupyterhub::jupyterhub_username,
+    require    => Python::Pyvenv[$jupyterhub::pyvenv],
   }
 
   python::pip { 'sudospawner':
     pkgname    => 'git+https://github.com/jupyter/sudospawner',
-    virtualenv => "$::jupyterhub::pyvenv",
-    owner      => $::jupyterhub::jupyterhub_username,
-    require    => Python::Pyvenv[ $::jupyterhub::pyvenv ],
+    virtualenv => $jupyterhub::pyvenv,
+    owner      => $jupyterhub::jupyterhub_username,
+    require    => Python::Pyvenv[$jupyterhub::pyvenv],
   }
 
   ensure_packages(['npm', 'nodejs-legacy'])
-  ->
-  exec { '/usr/bin/npm install -g configurable-http-proxy':
+
+  -> exec { '/usr/bin/npm install -g configurable-http-proxy':
     unless => '/usr/bin/npm list -g configurable-http-proxy',
   }
 }
